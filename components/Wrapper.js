5fc73f380cdf3860288c5aa64dc5e105d47d1c97
Arian ata <101602097+arianata@users.noreply.github.com>
2022-12-20 00:07:28 +0330

    Add files via upload
    For Additional Info Refer to ReadMe File

---
+++ components/Wrapper.js
@@ -0,0 +1,7 @@
+import "./Wrapper.css";
+
+const Wrapper = ({ children }) => {
+  return <div style={{marginRight:775,marginTop:130}} className="wrapper">{children}</div>;
+};
+
+export default Wrapper;
